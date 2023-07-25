7bb82b6eaf4d4cdeaa31084a2931e2c85269f172
GE-P <83733947+GE-P@users.noreply.github.com>
2023-07-25 16:50:15 +0200

    format: formated code

--- src/app.py
+++ src/app.py
@@ -19,6 +19,7 @@ def index():
     entries = feed.entries
     entries_by_date = group_entries_by_date(entries)
     return render_template('index.html', entries_by_date=entries_by_date)
+    
 
 def group_entries_by_date(entries):
     entries_by_date = defaultdict(list)
@@ -28,10 +29,10 @@ def group_entries_by_date(entries):
         published_date = parse_date(entry.published)
         entries_by_date[published_date].append(entry)
     return entries_by_date
+    
 
 def parse_date(date_str):
     return datetime.strptime(date_str, "%a, %d %b %Y %H:%M:%S %z").strftime("%Y-%m-%d")
-
 
 
 def is_image_accessible(url):
@@ -53,7 +54,7 @@ def get_article_image(url):
                 # return image_url # --> uncomment this to return the scraped images if available
                 return PLACEHOLDER_IMAGE_URL  # --> and comment this one
     return PLACEHOLDER_IMAGE_URL
-
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
